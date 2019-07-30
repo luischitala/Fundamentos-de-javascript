1e7e6c929df50043f86f15db62234a92af1b2f8e
Luis Chitala <itastetaco@gmail.com>
2019-07-29 19:50:06 -0500

    Alcance de funciones

---
+++ clase5.js
@@ -0,0 +1,13 @@
+var nombre = "Luis"
+
+function imprimirNombreEnMayusculas(nombre)
+{
+	window.nombre
+	nombre= nombre.toUpperCase()
+	console.log(nombre)
+
+}
+
+imprimirNombreEnMayusculas(nombre)
+
+
