874db81ea47ae2b10c985e0266da18903f497a75
Luis Chitala <itastetaco@gmail.com>
2019-07-30 00:46:30 -0500

    Ciclo final switch con default y breaks

---
+++ switch.js
@@ -0,0 +1,22 @@
+var signo = prompt("¿Cual es tu signo?")
+
+switch(signo)
+{
+	case "acuario":
+	console.log("Intente no cometer ningún error a causa de su impaciencia. Antes que nada relájese y podrá conseguir lo que se proponga sin ningún problema.")
+	break
+
+	case "piscis":
+	console.log("Aproveche que su mente estará activa y podrá incrementar la inteligencia. De esta forma, podrá realizar cualquier actividad sin tener mucho esfuerzo.")
+	break
+
+	case "leo":
+	console.log("Seguramente en este día, se despierte con muy pocas ganas de cumplir con las responsabilidades diarias. Entienda que no es momento para decaer.")
+	break
+
+	default:
+	console.log("No es un signo zodiacal valido")
+	break
+}
+
+
