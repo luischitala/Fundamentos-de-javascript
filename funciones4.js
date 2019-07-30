73ad0e89850ce2ef44b6fb47271974b126b963c3
Luis Chitala <itastetaco@gmail.com>
2019-07-29 19:39:00 -0500

    Primera practica de funciones

---
+++ funciones4.js
@@ -0,0 +1,11 @@
+var nombre = "Luis", edad = 28
+
+function imprimirEdad (n,e)
+{
+	console.log(`${n} tiene ${e} anos`)
+}
+
+imprimirEdad(nombre,edad)
+imprimirEdad("Vicky",28)
+imprimirEdad("Eric",22)
+imprimirEdad("Dario",21)
