352b1335cfefff7b34e2c0014fba02a9af34b01a
Luis Chitala <itastetaco@gmail.com>
2019-07-29 19:05:32 -0500

    inicio con clases de variables y strings

---
+++ clase1.js
@@ -0,0 +1,7 @@
+var nombre = "Luis", apellido = "Chitala"
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetras = nombre.length
+var nombreCompleto = `${nombre} ${apellido}`
+var str = nombre.charAt(1) + nombre.charAt(2)
