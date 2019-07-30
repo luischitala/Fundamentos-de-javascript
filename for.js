4d29dae83c514b73d20f2787b837844fefa18c21
Luis Chitala <itastetaco@gmail.com>
2019-07-29 23:53:07 -0500

    Se cambio el ciclo for por el while con arrow functions

--- for.js
+++ for.js
@@ -11,22 +11,26 @@ console.log(`Al inicio del año ${luis.nombre} pesa ${luis.peso.toFixed(2)}kg`)
 // function aumentarDePeso (persona)
 //{return persona.peso += 200
 //}
-const INCREMENTO_PESO = 0.2
+const INCREMENTO_PESO = 0.3
 const DIAS_DEL_ANO = 365
 const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
 const adelgazar = persona => persona.peso -=INCREMENTO_PESO
-for(var i = 1; i <= DIAS_DEL_ANO; i++)
-{
-	var random = Math.random()
-	if (random < 0.25)
-	{
-		aumentarDePeso(luis)
-	}
-	else if (random < 0.25)
-	{
-		adelgazar(luis)
-	}
-}
+const comeMucho  = () => Math.random()< 0.3
+const realizaDeporte = () => Math.random() < 0.4
+const META = luis.peso - 3
+var dias = 0
 
+while (luis.peso > META){
+	
+	if(comeMucho()){
+	aumentarDePeso(luis)
+	}
+	if(realizaDeporte())
+	{
+	adelgazar(luis)
+	}
+	dias += 1
 
-console.log(`Al final del año ${luis.nombre} pesa ${luis.peso.toFixed(2)} kg`)
+	}
+
+console.log(`Pasaron ${dias} dias hasta que ${luis.nombre} adelgazo 3 kg`)
