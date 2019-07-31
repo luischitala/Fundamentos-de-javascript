24cbf10ea33e987f531046f921e07a9b9f0c0af3
Luis Chitala <itastetaco@gmail.com>
2019-07-30 22:18:29 -0500

    Correcta filtracion de un array

--- arrays.js
+++ arrays.js
@@ -33,10 +33,35 @@ var miguel = {
 	apellido: "Vega",
 	estatura: 1.70
 }
+// si se quiere especificar mas
+// const esAlta =({altura}) => altura > 1.6
+
+// var personasAltas = personas.filter(function (persona))
+//{
+//	return persona.altura > 1.6s
+//}
+
 
 var personas = [luis, montse, maritza, cesar, eduardo, miguel]
+
+
+const esAlta = (persona) => persona.estatura > 1.6
+const esBaja = (persona) => persona.estatura < 1.6
+var personasBajas = personas.filter(esBaja)
+var personasAltas = personas.filter(esAlta)
+
+console.log(personasAltas)
+console.log(personasBajas)
+
 for (var i = 0; i < personas.length; i++)
 {
 	var persona = personas[i]
-	console.log(`${personas.nombre} mide ${persona.estatura} metros `)
+	console.log(`${persona.nombre} mide ${persona.estatura} metros `)
+}
+
+console.log(personasBajas)
+for (var i = 0; i < personas.length; i++)
+{
+	var persona = personas[i]
+	console.log(`${persona.nombre} mide ${persona.estatura} metros `)
 }
