13e84a4d9d270cf5c74b1e61cfd4d386ebb74aac
Luis Chitala <itastetaco@gmail.com>
2019-07-31 21:18:33 -0500

    Primer ejercicio de promesas

--- tiempo.js
+++ tiempo.js
@@ -4,40 +4,23 @@ const PEOPLE_URL = "people/:id"
 const opts = {crossDomain: true }
 
 
-function obtenerPersonaje(id,callback){
+function obtenerPersonaje(id){
+
+	return new Promise((resolve,reject) => {
 	const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
-
 	$
-		.get(url, opts,callback)
-		.fail(() => {
-		console.log(`sucedio un error. No se puedo obtener el personaje ${id}`)
-	})
-
-}
-
-obtenerPersonaje(1, function (personaje){
-	console.log(`Hola, yo soy ${personaje.name}`)
-
-	obtenerPersonaje(2, function(personaje){
-		console.log(`Hola, yo soy ${personaje.name}`)
-
-		obtenerPersonaje(3, function(personaje){
-			console.log(`Hola, yo soy ${personaje.name}`)
-
-			obtenerPersonaje(4, function(personaje){
-				console.log(`Hola, yo soy ${personaje.name}`)
-
-				obtenerPersonaje(5,function(personaje){
-					console.log(`Hola, yo soy ${personaje.name}`)
-
-					obtenerPersonaje(6,function(personaje){
-						console.log(`Hola, yo soy ${personaje.name}`)
-					})
-				})
-
-			})
+		.get(url, opts, function(data) {
+			resolve(data)
 		})
-
+		.fail(() => reject(id))
 	})
-
+		
+}
+function onError(id){
+	console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
+obtenerPersonaje(1)
+.then(function(personaje){
+	console.log(`El personaje 1 es ${personaje.name}`)
 })
+.catch(onError)
