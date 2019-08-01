bc620580fbb6e4a144b03626a9dc0dbad79fa093
Luis Chitala <itastetaco@gmail.com>
2019-07-31 20:31:34 -0500

    Muestra del asincronismo de Js mediante los tiempos de muestra desordenado de los elemetos requeridos

--- tiempo.js
+++ tiempo.js
@@ -1,10 +1,16 @@
 const API_URL = "https://swapi.co/api/"
 const PEOPLE_URL = "people/:id"
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`
 const opts = {crossDomain: true }
-const onLukeResponse = function(persona){
+const onPeopleResponse = function(persona){
 	console.log(`Hola, yo soy ${persona.name}`)
 }
 
-$.get(lukeUrl,opts, onLukeResponse)
+function obtenerPersonaje(id){
+	const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
+	$.get(url, opts, onPeopleResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
