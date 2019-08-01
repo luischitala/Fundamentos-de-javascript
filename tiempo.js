f054d97daebe22a928a9c112ea764762f8708ddc
Luis Chitala <itastetaco@gmail.com>
2019-07-31 20:42:46 -0500

    Generacion del callback hell

--- tiempo.js
+++ tiempo.js
@@ -2,15 +2,28 @@ const API_URL = "https://swapi.co/api/"
 const PEOPLE_URL = "people/:id"
 
 const opts = {crossDomain: true }
-const onPeopleResponse = function(persona){
-	console.log(`Hola, yo soy ${persona.name}`)
-}
 
-function obtenerPersonaje(id){
+
+function obtenerPersonaje(id,callback){
 	const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
-	$.get(url, opts, onPeopleResponse)
+
+	$.get(url, opts, function(persona){
+	console.log(`Hola, yo soy ${persona.name}`)
+	if (callback){
+		callback()
+	}
+})
+
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function (){
+	obtenerPersonaje(2, function(){
+		obtenerPersonaje(3, function(){
+			obtenerPersonaje(4, function(){
+				
+			})
+		})
+
+	})
+
+})
