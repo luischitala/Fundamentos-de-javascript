8d9b523be7f1f5f851f5a29c859835f6ef492ede
Luis Chitala <itastetaco@gmail.com>
2019-07-31 21:00:13 -0500

    Solucion de problemas con .fail y re-escritura de funcion arrow

--- tiempo.js
+++ tiempo.js
@@ -7,20 +7,34 @@ const opts = {crossDomain: true }
 function obtenerPersonaje(id,callback){
 	const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`
 
-	$.get(url, opts, function(persona){
-	console.log(`Hola, yo soy ${persona.name}`)
-	if (callback){
-		callback()
-	}
-})
+	$
+		.get(url, opts,callback)
+		.fail(() => {
+		console.log(`sucedio un error. No se puedo obtener el personaje ${id}`)
+	})
 
 }
 
-obtenerPersonaje(1, function (){
-	obtenerPersonaje(2, function(){
-		obtenerPersonaje(3, function(){
-			obtenerPersonaje(4, function(){
-				
+obtenerPersonaje(1, function (personaje){
+	console.log(`Hola, yo soy ${personaje.name}`)
+
+	obtenerPersonaje(2, function(personaje){
+		console.log(`Hola, yo soy ${personaje.name}`)
+
+		obtenerPersonaje(3, function(personaje){
+			console.log(`Hola, yo soy ${personaje.name}`)
+
+			obtenerPersonaje(4, function(personaje){
+				console.log(`Hola, yo soy ${personaje.name}`)
+
+				obtenerPersonaje(5,function(personaje){
+					console.log(`Hola, yo soy ${personaje.name}`)
+
+					obtenerPersonaje(6,function(personaje){
+						console.log(`Hola, yo soy ${personaje.name}`)
+					})
+				})
+
 			})
 		})
 
